9b81270a423eb4608687c6aceb519e17d7ccdddd
pdcoder <pdas@inversion.co.in>
2019-03-19 18:13:39 +0530

    Added material d

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "./assets/scss/material-kit-react.scss?v=1.4.0";
+
+// pages for this product
+import Components from "./views/Components/Components.jsx";
+import LandingPage from "./views/LandingPage/LandingPage.jsx";
+import ProfilePage from "./views/ProfilePage/ProfilePage.jsx";
+import LoginPage from "./views/LoginPage/LoginPage.jsx";
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+var hist = createBrowserHistory();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
