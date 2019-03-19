67644c4cce4eecd1661b455c6c4b0d7fb3f77ca9
pdcoder <pdas@inversion.co.in>
2019-03-19 13:41:11 +0530

    Added material design

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import BankContract from "./contracts/Bank.json";
 import getWeb3 from "./utils/getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = {
+    admin: '',
+    customerCount: '',
+    balance: '',
+    contract: '',
+    web3: '',
+    accounts: [] 
+  };
 
   componentDidMount = async () => {
     try {
@@ -15,19 +22,19 @@ class App extends Component {
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = BankContract.networks[networkId];
+      console.log(deployedNetwork);
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        BankContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      const admin = await instance.methods.admin.call({from : accounts[0]});
+      this.setState({ web3, accounts, admin, contract: instance });
+
+      console.log(this.state);
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
@@ -35,18 +42,7 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
 
   render() {
     if (!this.state.web3) {
